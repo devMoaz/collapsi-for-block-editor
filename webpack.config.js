8faf9213012da551a9f3f10464216a0ee80f4003
Moaz <moaz.mahmoud.dev@gmail.com>
2026-06-04 17:14:25 +0300

    Update webpack.config.js comment to reflect new admin page label

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 /**
  * Webpack config. Extends @wordpress/scripts default to expose two entries:
  * - editor: loaded on edit-post / edit-site screens
- * - settings: loaded on the Settings > Block Collapser admin page
+ * - settings: loaded on the Settings > Collapsi admin page
  */
 const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
 const path = require( 'path' );
